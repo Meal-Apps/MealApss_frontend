ec1613b227d4b503e0052f301bc181b3bc243788
refathossen <refathossen2001@gmail.com>
2024-10-16 23:55:21 +0600

    home page design done

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,19 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import { Route, Routes } from 'react-router-dom'
 import Home from './pages/home'
+import Login from './pages/auth/manager/Login'
+import Register from './pages/auth/manager/Register'
+import axios from 'axios'
 
 function App() {
   const [count, setCount] = useState(0)
+  axios.defaults.baseURL = "http://127.0.0.1:8000/api/";
 
   return (
     <Routes>
       <Route exact path='/' element={<Home/>}></Route>
+      <Route exact path='/manager-login' element={<Login/>}></Route>
+      <Route exact path='/manager-register' element={<Register/>}></Route>
     </Routes>
   )
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,23 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 function Home() {
   return (
-    <div>bfbfbH
+    <div className='bg-gradient-to-r from-indigo-400 to-cyan-400'>
+      <div className="flex flex-col items-center justify-center h-screen">
+        <div className="flex space-x-8">
+          <Link to="/user-login" className="px-8 py-4 text-white bg-purple-500 rounded-lg hover:bg-purple-600 focus:outline-none focus:ring-2 focus:ring-purple-400 focus:ring-offset-2 transition duration-300 ease-in-out transform hover:-translate-y-1 hover:scale-105 shadow-lg inline-block text-center">
+            User Login
+          </Link>
+          <Link to="/manager-login" className="px-8 py-4 text-white bg-blue-500 rounded-lg hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-offset-2 transition duration-300 ease-in-out transform hover:-translate-y-1 hover:scale-105 shadow-lg inline-block text-center">
+            Manager Login
+          </Link>
+        </div>
+      </div>
 
-<h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+      
+
+
     </div>
   )
 }
